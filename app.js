3342a332df3ee1822fef0fd18d2a00c77e86fd00
Varad Vanjape <varadtech67@gmail.com>
2020-04-23 12:59:01 +0530

    implemented :- hold, created :- nextPlayer and init

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-/*
-RULES:
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-*/
-
 const electron = require('electron');
 const { app, ipcRenderer } = electron;
 const diceDOM = document.querySelector('.dice');
@@ -29,27 +20,68 @@ btnRollDOM.addEventListener('click', () => {
         roundscore += diceValue;
         document.querySelector('#current-' + activePlayer).innerHTML = '<em><b>' + roundscore + '</b></em>';
     } else {
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundscore = 0;
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
+       nextPlayer();
     }
 
 });
 
 btnHoldDOM.addEventListener('click', () => {
-    ipcRenderer.send('data-inc', "hello world");
+  if (gamePlaying) {
+        scores[activePlayer] += roundScore;
+
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+        if (scores[activePlayer] >= 100) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        } else {
+            nextPlayer();
+        }
+    }  
 
 });
 
-btnNewDOM.addEventListener('click', () => {
-    roundscore = 0;
-    scores[0] = 0, scores[1] = 0;
-    document.querySelector('#current-0').textContent = 0;
-    document.querySelector('#current-1').textContent = 0;
-    document.getElementById('dice').style.display = 'none';
+btnNewDOM.addEventListener('click', init);
 
-});
+
+function init() {
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+    gamePlaying = true;
+    
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
+
+
+function nextPlayer() {
+    //Next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    //document.querySelector('.player-0-panel').classList.remove('active');
+    //document.querySelector('.player-1-panel').classList.add('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
